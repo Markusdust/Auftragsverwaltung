331d05b611a1a96ef7deb470446f96d233b8597c
Andy Steingruber <steingruber.andy.gmail.com@zbw-online.ch>
2022-01-18 16:28:51 +0100

    Tabelle Auftrag und Position erstellt mit der dazugehörigen Konfiguration

---
+++ DataAccessLayer/EntityKonfigurationen/PositionKonfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccessLayer.EntityKonfigurationen
+{
+    public class PositionKonfiguration : IEntityTypeConfiguration<Position>
+    {
+        public void Configure(EntityTypeBuilder<Position> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.PositionNr).IsRequired().HasMaxLength(45);
+            builder.Property(x => x.Menge).IsRequired().HasMaxLength(45);
+        }
+    }
+}
